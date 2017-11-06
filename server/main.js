41a09b88adf0c2c526c9b6d01bc9b0f288b2691b
TobyGWilliams <Toby.G.Williams@Googlemail.com>
2017-11-06 09:29:11 +0000

    Updates

--- server/main.js
+++ server/main.js
@@ -2,23 +2,15 @@ const EventEmitter = require('events')
 const Countdown = require('./countdown.js')
 const Camera = require('./camera.js')
 const File = require('./file.js')
-const GPIO = require('./gpio.js')
+// const GPIO = require('./gpio.js')
 
-const filepath = 'c:/users/toby/pictures'
+const filepath = 'C:/Users/Toby/Pictures'
 
 var emitter = new EventEmitter()
 var file = new File(emitter, filepath)
 var countdown = new Countdown(emitter)
 var camera = new Camera(emitter)
-var GPIO = new GPIO(emitter, 18)
-
-emitter.on('tick-final', (d)=>{
-    console.log(d)
-})
-
-emitter.on('tick', (d)=>{
-    console.log(d)
-})
+// var gpioa = new GPIO(emitter, 18)
 
 emitter.on('file-new', (d)=>{
     console.log(d)

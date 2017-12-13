ef454abbc1b80843286515d6bdcbf4f01841b9e6
TobyGWilliams <Toby.G.Williams@Googlemail.com>
2017-12-13 00:24:31 +0000

    debounce

--- server/gpio.js
+++ server/gpio.js
@@ -1,13 +1,23 @@
+import { setTimeout } from 'timers';
+
 var gpio = require('rpi-gpio')
 
 function Button(emitter, pin) {
     this.emitter = emitter
     this.pin = pin
-    console.log('gpio.js',emitter, pin)
+    this.listening = true
+
+    console.log('gpio.js', emitter, pin)
 
     gpio.on('change', (c,v)=>{
         if(v==true){
             this.emitter.emit('gpio-press', this.pin)
+            console.log('disable gpio listen')
+            this.listening = false
+            setTimeout(()=>{
+                console.log('re-enable gpio listen')
+                this.listening = true
+            },5000)
         }
     })
     gpio.setup(this.pin, gpio.DIR_IN, gpio.EDGE_BOTH)

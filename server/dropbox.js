5aa84f447ba4113d24a9aae09d23ca93925a5303
Toby Williams <Toby.Williams@waitrose.co.uk>
2018-12-11 00:09:24 +0000

    dropbox format

--- server/dropbox.js
+++ server/dropbox.js
@@ -1,15 +1,12 @@
 /* eslint-env node */
-const Dbx = require('dropbox');
-const fs = require('fs');
+import Dbx from 'dropbox';
+import fs from 'fs';
+import EventEmitter from 'events';
 
 const clientID = '4210jna60uh85i9';
 
-/**
- * Communicates with Dropbox.
- * @param {emitter} emitter - emitter that the application communicates over.
- */
-function Dropbox(emitter) {
-  this.emitter = emitter;
+function Dropbox() {
+  this.emitter = new EventEmitter();
 
   this.status = 'not-auth';
   this.name = '';
@@ -18,8 +15,14 @@ function Dropbox(emitter) {
   this.dbx = new Dbx();
   this.dbx.setClientId(clientID);
 
-  this.emitter.on('config-get', ()=>{
-    this.emitter.emit('dropbox-authUrl', this.dbx.getAuthenticationUrl('http://localhost:3000/dropbox', 'response_type=code'));
+  this.emitter.on('config-get', () => {
+    this.emitter.emit(
+      'dropbox-authUrl',
+      this.dbx.getAuthenticationUrl(
+        'http://localhost:3000/dropbox',
+        'response_type=code'
+      )
+    );
     this.emitter.emit('dropbox-authStatus', {
       status: this.status,
       name: this.name,
@@ -27,29 +30,32 @@ function Dropbox(emitter) {
     });
   });
 
-  this.emitter.on('dropbox-new-picture', (e)=> {
+  this.emitter.on('dropbox-new-picture', (e) => {
     if (this.status == 'auth') {
       let file = fs.readFileSync(e);
-      this.dbx.filesUpload({path: '/test_folder/filename.jpg', contents: file})
-      .then((r) => {
-        this.emitter.emit('dropbox-upload-success');
-        this.dbx.sharingCreateSharedLink({path: r.path_lower}).then((r) => {
-          this.emitter.emit('dropbox-url', r.url);
+      this.dbx
+        .filesUpload({path: '/test_folder/filename.jpg', contents: file})
+        .then((r) => {
+          this.emitter.emit('dropbox-upload-success');
+          this.dbx.sharingCreateSharedLink({path: r.path_lower}).then((r) => {
+            this.emitter.emit('dropbox-url', r.url);
+          });
+        })
+        .catch((r) => {
+          this.emitter.emit('dropbox-upload-failure');
         });
-      }).catch((r)=>{
-        this.emitter.emit('dropbox-upload-failure');
-      });
     } else {
       console.log('dropbox - unable to upload picture');
     }
   });
 
-  this.emitter.on('dropbox-token', (e) =>{
-    if (e.match('access_token=(.*?)\&') != null) {
-      const token = e.match('access_token=(.*?)\&')[1];
+  this.emitter.on('dropbox-token', (e) => {
+    if (e.match('access_token=(.*?)&') != null) {
+      const token = e.match('access_token=(.*?)&')[1];
       this.dbx.setAccessToken(token);
-      this.dbx.usersGetCurrentAccount()
-      .then((r)=>{
+      this.dbx
+        .usersGetCurrentAccount()
+        .then((r) => {
           this.status = 'auth';
           this.email = r.email;
           this.name = r.name.display_name;
@@ -60,14 +66,13 @@ function Dropbox(emitter) {
             email: this.email,
           });
         })
-        .catch((r)=>{
+        .catch((r) => {
           console.log('dropbox-login-failure');
-        }
-      );
+        });
     } else {
       console.log('no token found');
     }
   });
-};
+}
 
 module.exports = Dropbox;

cedbbafde0e9df21df9e81709d7df54899a71212
Toby Williams <Toby.Williams@waitrose.co.uk>
2018-12-11 01:25:46 +0000

    File module refactor

--- server/file.js
+++ server/file.js
@@ -1,26 +1,21 @@
-const os = require('os')
-const path = require("path");
-const fs = require('fs')
+import EventEmitter from 'events';
+import fs from 'fs';
 
-class File{
-    constructor(e, f) {
-        this.emitter = e
-        this.filepath = f
-
-        console.log('File.js', this.emitter, this.filepath)
-
-        fs.watch(this.filepath, (event, filename) => {
-            // console.log('change detected', event, filename, this.filepath)
-            setTimeout(() => {
-                const regexFileName = RegExp('img(.*).jpg')
-                // console.log('callback', event, filename, this.filepath, fs.existsSync(this.filepath + "/"+ filename))
-                if (event == 'rename' && regexFileName.test(filename) && fs.existsSync(this.filepath + "/"+ filename)) {
-                    console.log('new file!')
-                    this.emitter.emit('file-new', filename)
-                }
-            }, 50)
-        });
-    }
+export class File {
+  constructor(filePath) {
+    this.emitter = new EventEmitter();
+    this.filepath = filePath;
+    fs.watch(this.filepath, (event, filename) => {
+      setTimeout(() => {
+        const regexFileName = RegExp('img(.*).jpg');
+        if (
+          event == 'rename' &&
+          regexFileName.test(filename) &&
+          fs.existsSync(this.filepath + '/' + filename)
+        ) {
+          this.emitter.emit('file-new', filename);
+        }
+      }, 50);
+    });
+  }
 }
-
-module.exports = File

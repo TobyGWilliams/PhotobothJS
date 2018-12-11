765af285d16cdaac5dd01889704144da4e058e24
Toby Williams <Toby.Williams@waitrose.co.uk>
2018-12-11 01:26:03 +0000

    what ever

--- server/test/test-file.js
+++ server/test/test-file.js
@@ -1,31 +1,7 @@
-const os = require('os')
-const path = require("path");
-const fs = require('fs')
+import File from '../file';
 
-const EventEmitter = require('events')
-const Countdown = require('./countdown.js')
-const Camera = require('./camera.js')
-const File = require('./file.js')
-const Server = require('./server.js')
-const JsonFile = require('jsonfile')
-const Socket = require('./socket.js')
+const file = new File('/Users/towillia/Documents/Photobooth');
 
-const config_file = './server/config.json'
-var config = JsonFile.readFileSync(config_file)
-
-var server = new Server('C:/Users/Toby/Pictures/Google Photos Backup')
-var emitter = new EventEmitter()
-var file = new File(emitter, config.file_path)
-var countdown = new Countdown(emitter)
-var camera = new Camera(emitter)
-
-var serv = server.server
-var socket = new Socket(emitter, serv)
-
-
-
-
-setInterval(()=>{
-  countdown.start(10)
-}, 20000)
-
+file.emitter.on('file-new', (fileName) => {
+  console.log('new file', fileName);
+});

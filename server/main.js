c793e988a11b98d5744952e49b4b7f67f46a59c5
Toby Williams <Toby.Williams@waitrose.co.uk>
2018-12-14 18:41:10 +0000

    fix import

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ import Server from './server';
 import {Camera} from './camera';
 import {Countdown} from './countdown';
 import {Dropbox} from './dropbox';
-import {Button} from '../gpio';
+import {Button} from './gpio';
 
 const configFile = './server/config.json';
 

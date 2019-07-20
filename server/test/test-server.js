8d4bc4d033e09b478531f9877c5876cb2514232d
Toby Williams <Toby.Williams@waitrose.co.uk>
2019-07-20 01:21:26 +0100

    delete old files

--- server/test/test-server.js
+++
@@ -1,7 +0,0 @@
-const config_file = './server/config.json'
-
-const EventEmitter = require('events')
-const Server = require('./server.js')
-
-var server = new Server('C:/Users/Toby/Pictures/Google Photos Backup')
-
